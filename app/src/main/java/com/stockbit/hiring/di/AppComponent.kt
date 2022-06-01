57300e08ca8640a21f44bef7f44c343ae2af44b7
Rizky Alfa Uji Gultom <rizkyalfauji11@gmail.com>
2022-06-01 09:21:53 +0700

    Initial Commit

---
+++ app/src/main/java/com/stockbit/hiring/di/AppComponent.kt
@@ -0,0 +1,8 @@
+package com.stockbit.hiring.di
+
+import com.stockbit.local.di.localModule
+import com.stockbit.remote.di.createRemoteModule
+import com.stockbit.repository.di.repositoryModule
+
+val appComponent =
+    listOf(createRemoteModule("https://api.github.com/"), repositoryModule, localModule)
